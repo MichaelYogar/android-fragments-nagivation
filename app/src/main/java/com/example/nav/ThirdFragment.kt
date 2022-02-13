fc06c552ad6251989fbc1aff033ee31b8bff09f3
Michael Yogar <michaelyogar878@gmail.com>
2022-02-12 22:45:39 -0500

    example of fragments with navigation

---
+++ app/src/main/java/com/example/nav/ThirdFragment.kt
@@ -0,0 +1,33 @@
+package com.example.nav
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class ThirdFragment : Fragment() {
+
+    private lateinit var thirdButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_third, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        thirdButton = view.findViewById(R.id.button_third)
+        thirdButton.setOnClickListener {
+            findNavController().navigate(R.id.action_ThirdFragment_to_FirstFragment)
+        }
+    }
+}
--- app/src/test/java/com/example/nav/ExampleUnitTest.kt
+++ app/src/test/java/com/example/nav/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.fragmentsnavigation
+package com.example.nav
 
 import org.junit.Test
 
